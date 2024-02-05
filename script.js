e761eec91df399b8ba37ef04ca0346e01bf763db
satyaprakashdhfm34 <159006547+satyaprakashdhfm34@users.noreply.github.com>
2024-02-05 14:26:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Function to handle button click
+function handleClick() {
+    alert("Button clicked!");
+}
+
+// Attach the function to the button click event
+document.getElementById("myButton").addEventListener("click", handleClick);
+
